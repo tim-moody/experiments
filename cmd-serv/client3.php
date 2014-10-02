b9237887a562a5ee01754ef5e5597e3cb7caa661
Tim Moody <tim@timmoody.com>
2014-10-02 17:47:48 -0400

    deleted a line

--- cmd-serv/client3.php
+++ cmd-serv/client3.php
@@ -11,7 +11,10 @@
 		<p>This is being served from xsce-cmdsrv using PHP.</p>
 
 <?php
+//  Socket to talk to server
 $context = new ZMQContext();
+$requester = new ZMQSocket($context, ZMQ::SOCKET_REQ);
+$requester->connect("ipc:///run/cmdsrv_sock");
 $requester->send("Hello");
 $reply = $requester->recv();
 
