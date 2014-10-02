0e7e0b715d763151565ee785d7785f75cf7f5aa8
Tim Moody <tim@timmoody.com>
2014-10-02 10:30:39 -0400

    and for client

---
+++ cmd-serv/client3.py
@@ -0,0 +1,17 @@
+import zmq
+import sys
+
+ipc_sock = "/tmp/cmdsrv_sock"
+
+context = zmq.Context()
+print "Connecting to server..."
+socket = context.socket(zmq.REQ)
+socket.connect ("ipc://%s" % ipc_sock)
+
+#  Do 5 requests, waiting each time for a response
+for request in range (1,5):
+    print "Sending request ", request,"..."
+    socket.send ("Hello")
+    #  Get the reply.
+    message = socket.recv()
+    print "Received reply ", request, "[", message, "]"
