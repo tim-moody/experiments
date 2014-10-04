ad7878fa3c8a6f941a9f22db0a3544519e5dbef1
Tim Moody <tim@timmoody.com>
2014-10-04 15:13:35 -0400

    add identity uuid to client5.py

---
+++ cmd-serv/prototype1/client5.py
@@ -0,0 +1,20 @@
+import zmq
+import sys
+import uuid
+
+ipc_sock = "/run/cmdsrv_sock"
+identity = uuid.uuid4()
+
+context = zmq.Context()
+print "Connecting to server..."
+socket = context.socket(zmq.REQ)
+socket.identity = identity.encode('ascii')
+socket.connect ("ipc://%s" % ipc_sock)
+
+#  Do 5 requests, waiting each time for a response
+for request in range (1,5):
+    print "Sending request ", request,"..."
+    socket.send ("Hello")
+    #  Get the reply.
+    message = socket.recv()
+    print "Received reply ", request, "[", message, "]"
