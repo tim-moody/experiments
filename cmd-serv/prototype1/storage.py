7ee677f0747f3241add4f1b1571caf395203f154
Tim Moody <tim@timmoody.com>
2014-10-05 13:40:33 -0400

    storage.py prototype

--- cmd-serv/prototype1/storage.py
+++ cmd-serv/prototype1/storage.py
@@ -1,10 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('queue.db')
 print "Opened database successfully";
 conn.row_factory = sqlite3.Row
 
-c = conn.execute("SELECT rowid, firstname, lastname FROM person")
+c = conn.execute("SELECT max (rowid) from commands")
 r = c.fetchone()
 print r
 print r.keys()
