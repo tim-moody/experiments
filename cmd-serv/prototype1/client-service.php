f47b09845e325da2c3c4094994bcfa58ff2985fa
Tim Moody <tim@timmoody.com>
2014-10-09 17:41:01 -0400

    added client-service.php

---
+++ cmd-serv/prototype1/client-service.php
@@ -0,0 +1,24 @@
+<?php
+/*
+*  xsce-cmdsrv client
+*  Connects REQ socket to ipc:///run/cmdsrv_sock
+*  Sends command, expects response back
+*/
+?>
+<html> 
+	<body> 
+		<h1>Hello world!</h1> 
+		<p>This is being served from xsce-cmdsrv using PHP.</p>
+
+<?php
+//  Socket to talk to server
+$context = new ZMQContext();
+$requester = new ZMQSocket($context, ZMQ::SOCKET_DEALER);
+$requester->connect("ipc:///run/cmdsrv_sock");
+$requester->send("Hello");
+$reply = $requester->recv();
+
+echo "message: $reply";
+?>
+  </body>
+</html>
