c80414c62c0eef91417bdacbe337c2ffb3001ba8
Tim Moody <tim@timmoody.com>
2014-10-09 18:02:23 -0400

    split hex

--- cmd-serv/prototype1/client-service.php
+++ cmd-serv/prototype1/client-service.php
@@ -27,7 +27,7 @@ echo strToHex($reply);
 function strToHex($string){
     $hex='';
     for ($i=0; $i < strlen($string); $i++){
-        $hex .= dechex(ord($string[$i]));
+        $hex .= " " . dechex(ord($string[$i]));
     }
     return $hex;
 }
